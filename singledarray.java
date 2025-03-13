5f916888a4a7e760b7fd2ce0b8f58268daf34172
sathwika300 <sathwikarentala@gmail.com>
2025-03-13 11:34:43 +0530

    Add files via upload

---
+++ singledarray.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+class singledarray
+{
+public static void main(String args[])
+{
+int marks[]=new int [6];
+marks[0]=60;
+marks[1]=70;
+marks[2]=65;
+marks[3]=75;
+marks[4]=80;
+marks[5]=89;
+for(int i=0; i<marks.length;i+1)
+{
+System.out.println(marks[i]));
+}
+}
+}
